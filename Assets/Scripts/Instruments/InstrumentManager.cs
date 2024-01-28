7386912ac8c14f2e3ae7303fc32f9801e032b7b5
sdfdanielsoto <danielsoto1995@hotmail.com>
2024-01-28 09:42:58 +0100

    sounds

--- Assets/Scripts/Instruments/InstrumentManager.cs
+++ Assets/Scripts/Instruments/InstrumentManager.cs
@@ -1,4 +1,5 @@
 
+using MJW.Audio;
 using MJW.Game;
 using MJW.Utils;
 using System.Collections.Generic;
@@ -21,6 +22,8 @@ namespace MJW.Instruments
 
         private InstrumentType _currentInstrument;
         private int _currentNotesAmount;
+        private int _currentNoteIndex;
+        private InstrumentSheet _currentSheet;
 
         #endregion
 
@@ -46,24 +49,116 @@ namespace MJW.Instruments
 
         public void OnNoteSuccess(InstrumentType instrument)
         {
+            if (_currentNoteIndex < _currentSheet.Notes.Count)
+            {
+                AudioManager.Instance.PlaySFX(_currentSheet.Notes[_currentNoteIndex]);
+            }
+            else if (_currentNoteIndex-1 < _currentSheet.Notes.Count)
+            {
+                AudioManager.Instance.PlaySFX(_currentSheet.Notes[_currentNoteIndex-1]);
+            }
 
+            _currentNoteIndex++;
         }
 
         public void OnNoteFailed(InstrumentType instrument)
         {
-
+            AudioManager.Instance.PlaySFX(GetFailSound(instrument));
+            _currentNoteIndex++;
         }
 
         public void OnSheetGenerated(int notes, InstrumentType instrument)
         {
-
+            _currentNotesAmount = notes;
+            _currentInstrument = instrument;
+            _currentNoteIndex = 0;
+            _currentSheet = Find(instrument, notes);
         }
 
         public void OnSheetCompleted(int errors, InstrumentType instrument)
         {
-
+            AudioManager.Instance.PlaySFX(GetOkSound(instrument));
         }
 
         #endregion
+
+        private InstrumentSheet Find(InstrumentType instrument, int Notes)
+        {
+            var sheets = new List<InstrumentSheet>();
+
+            switch (instrument)
+            {
+                case InstrumentType.Banjo:
+                    foreach(var banjo in _banjoSheets)
+                    {
+                        if (banjo.Notes.Count == Notes)
+                        {
+                            sheets.Add(banjo);
+                        }
+                    }
+                    break;
+
+                case InstrumentType.Tambor:
+                    foreach (var perc in _percSheets)
+                    {
+                        if (perc.Notes.Count == Notes)
+                        {
+                            sheets.Add(perc);
+                        }
+                    }
+                    break;
+
+                case InstrumentType.Trompeta:
+                    foreach (var xyl in _xylSheets)
+                    {
+                        if (xyl.Notes.Count == Notes)
+                        {
+                            sheets.Add(xyl);
+                        }
+                    }
+                    break;
+            }
+
+            if (sheets.Count > 0)
+            {
+                return sheets[Random.Range(0, sheets.Count)];
+            }
+
+            return new InstrumentSheet();
+        }
+
+        private SoundType GetOkSound(InstrumentType type)
+        {
+            switch (type)
+            {
+                case InstrumentType.Banjo:
+                    return SoundType.banjoOk;
+
+                case InstrumentType.Tambor:
+                    return SoundType.percOk;
+
+                case InstrumentType.Trompeta:
+                    return SoundType.xylOk;
+            }
+
+            return SoundType.Undefined;
+        }
+
+        private SoundType GetFailSound(InstrumentType type)
+        {
+            switch (type)
+            {
+                case InstrumentType.Banjo:
+                    return SoundType.banjoFail;
+
+                case InstrumentType.Tambor:
+                    return SoundType.percFail;
+
+                case InstrumentType.Trompeta:
+                    return SoundType.xylFail;
+            }
+
+            return SoundType.Undefined;
+        }
     }
 }
